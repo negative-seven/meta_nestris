d773caff4d7df748f1b83a8d5d66c4a42f3da1e0
negative-seven <21268649+negative-seven@users.noreply.github.com>
2023-01-05 21:05:19 +0100

    Verify that meta state inputs length matches its frame count

--- tests/meta_state_equivalent.rs
+++ tests/meta_state_equivalent.rs
@@ -4,6 +4,7 @@ use meta_nestris::{game_type::GameType, meta_state::MetaState, state::State};
 fn meta_state_equivalent() {
     let meta_state = MetaState::new(0, GameType::A);
     let inputs = meta_state.get_inputs();
+    assert_eq!(inputs.len(), meta_state.frame.try_into().unwrap());
 
     let gameplay_state_from_meta_state = meta_state.to_gameplay_state();
 
